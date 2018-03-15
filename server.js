f93ab4e9268d13aa1314787af3af097d58f2649d
Aditi Mishra <asmishra93@gmail.com>
2018-03-14 21:52:26 -0500

    Setting up server.js and adding to .gitignore

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// serve static content from the public directory
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// setting up handlebars in case we decide to use it
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+// requiring our models for syncing
+var db = require('./models');
+
+// once we've created our route files, require them here
+
+// syncing our sequelize models and then starting our Express app
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+});

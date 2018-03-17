f40bd350d3c1cc456cea17c5ccc5b2cfc9967a10
Scott Saltzman <salty923@gmail.com>
2018-03-16 23:06:05 -0500

    add gecode for lat long

--- public/assets/js/runchi.js
+++ public/assets/js/runchi.js
@@ -1,15 +1,27 @@
 
 $(document).ready(function () {
+    var geocoder;
 
     $('#submit').on('click',function () {
-        var user = $('#userName').val().trim();
+        event.preventDefault();
+        var user = $('#user').val();
         var date = $('#date').val().trim();
         var time = $('#time').val().trim();
         var address =$('#addr').val().trim();
-        var pace= $('#pace').val().trim();
+        //var pace= $('#pace').val().trim();
         var message = $('#message').val().trim();
 
-        alert(`${user}  ${date}  ${time}  ${address}  ${pace} ${message} `);
+        geocoder = new google.maps.Geocoder();
+        geocoder.geocode({ 'address': address }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+
+                console.log('geocoder results:');
+                console.log(results);
+            }
+        });
+
+
+        console.log(`${user}  ${date}  ${time}  ${address} ${message} `);
     })
 
 
--- schema.sql
+++ schema.sql
@@ -1,2 +1,4 @@
 drop database if exists runChi_db;
 create database runChi_db;
+
+USE runChi_db;

97e716e3a54e3e744159590db0a3a174108ad031
Aditi Mishra <asmishra93@gmail.com>
2018-03-17 10:42:04 -0500

    Serving create run page

--- controllers/runController.js
+++ controllers/runController.js
@@ -4,11 +4,16 @@ var user = require('../models/user.js');
 
 var path = require('path');
 
+// remember to change file names once updated info is available
 module.exports = function (app) {
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, '../views/index2_DoNotDelete.html'));
     })
 
+    
+    app.get('/createrun', function (req, res) {
+        res.sendFile(path.join(__dirname, '../views/createruncopy.html'));
+    })
 }
 
 

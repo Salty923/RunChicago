2466d254254047c9e8a7106729194cef3bef178c
Brooke Wildermuth <wildermuthbrooke@gmail.com>
2018-03-20 22:51:59 -0500

    Sign-Up/Login logic

--- controllers/runController.js
+++ controllers/runController.js
@@ -1,9 +1,36 @@
 // importing models to use their database functions
-var db = require('../models/')
+var db = require('../models/');
 var path = require('path');
+var passport = require('../config/passport');
 
 // remember to change file names once updated info is available
 module.exports = function (app) {
+    app.post("/api/signup", function(req, res) {
+        console.log(req.body);
+        db.User.create({
+          email: req.body.email,
+          password: req.body.password
+        }).then(function() {
+            res.json("/success")
+        //   res.redirect(307, "/api/login");
+        }).catch(function(err) {
+          console.log(err);
+          res.json(err);
+          // res.status(422).json(err.errors[0].message);
+        });
+      });
+      
+    app.post("/api/login", function(req, res, next) {
+        passport.authenticate('local', function(err, user, info) {
+          if (err) { return next(err); }
+          if (!user) { return res.send('/fail'); }
+          req.logIn(user, function(err) {
+            if (err) { return next(err); }
+            // return res.redirect('/users/' + user.username);
+            return res.json("/success");
+          });
+        })(req, res, next);
+      });
 
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, '../views/index.html'));
